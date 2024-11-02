affe79c35edadc67c2fc37113d03a787e64f3e5c
ahmadfadhil221231 <ahmadfadhil221231@gmail.com>
2024-11-02 23:48:50 +0700

    Create cpanel.php

---
+++ cpanel.php
@@ -0,0 +1,7 @@
+
+<html> <head> 	 <title>Cpanel Crack ~ Palembang Cyber Team</title> 	 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 		 <style> 		.draw {
+  height:45px; 
+}
+.wis{
+  height: 25px;
+}body { 	 color:#80D713; 	 background-color:#f0eff0;; 	 } 		.logo{ height:70px;}	 			 input[type="email"] { border-radius:5px; border-bottom-color: blue; border-top-color: blue; padding: 10; } 		 </style> 	</head> <body> 	 <br><br><br><br> <center>	<div style=" border: 7px double; border-color: #F90000;border-radius: 16px 16px 16px 16px;-moz-border-radius: 16px 16px 16px 16px;-webkit-border-radius: 16px 16px 16px 16px;height:auto;width:500px; background-color:black;  color:white;"><br><br> <img class="logo"src="https://1.top4top.net/p_13465und10.jpg">	 <br><br><img src="https://2.top4top.net/p_1346rvvr31.jpg" class="draw"> 	 <br><br>  <form action="#" method="post"> 	<input type="email" name="email" placeholder="Email Kau Ler" style="background-color: " /> 	<input type="submit" name="submit" value="Crack" style="background-color: transparent;font: 15pt cursive;color:#80D713;"/> 	</form> 	 	<br /><br /><br /> 	</p> 	 </div> </div></center> </body> </html>
