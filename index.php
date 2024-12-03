ed56d1c9bf48bbcb19ed671d899bbb0af37f0447
ahmadfadhil221231 <ahmadfadhil221231@gmail.com>
2024-12-03 21:41:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
-ErrorDocument 404 https://domikado.com/
-ErrorDocument 500 https://domikado.com/
+ErrorDocument 404 https://www.brilliantdadashova.com/
+ErrorDocument 500 https://www.brilliantdadashova.com/
 # or map them to one error document:
 # ErrorDocument 404 /pages/errors/error_redirect.php
 # ErrorDocument 500 /pages/errors/error_redirect.php
@@ -8,10 +8,10 @@ RewriteEngine On
 RewriteBase /
 
 RewriteCond %{REQUEST_URI} ^/404/$
-RewriteRule ^(.*)$ https://domikado.com/ [L]
+RewriteRule ^(.*)$ https://www.brilliantdadashova.com/ [L]
 
 RewriteCond %{REQUEST_URI} ^/500/$
-RewriteRule ^(.*)$ https://domikado.com/ [L]
+RewriteRule ^(.*)$ https://www.brilliantdadashova.com/ [L]
 
 # or map them to one error document:
 #RewriteCond %{REQUEST_URI} ^/404/$ [OR]
